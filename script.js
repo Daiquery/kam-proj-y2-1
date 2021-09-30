0c3401e4a83cec4b43545f229f554c4ada921f60
Nicholas Burney <nicholasburney@The-Ultimatum.local>
2021-09-29 22:53:19 -1000

    reccomm

---
+++ script.js
@@ -0,0 +1,72 @@
+var items = [];
+function createItem() {
+  if (items.length >= 4) {
+    console.log("List is full!");
+    document.getElementById("warn").innerText =
+      "List is full! Remove an item before continuing.";
+    return;
+  }
+
+  document.getElementById("warn").innerText = "";
+
+  var item = document.getElementById("listInput").value;
+
+  for (i = 0; i < items.length; i++) {
+    if (items[i] === item) {
+      document.getElementById("warn").innerText =
+        "You already have this item on the list..";
+      return;
+    }
+  }
+
+  items = [...items, item];
+  var table = document.getElementById("awesome");
+
+  index = items.findIndex((x) => x === item);
+
+  var order = index + 1;
+  var newRow = document.createElement("tr");
+  var indexData = document.createElement("td");
+  var itemData = document.createElement("td");
+  // append 2 pieces of data to new row,
+  indexData.innerHTML = `${order}`;
+  itemData.innerHTML = `${item}`;
+
+  newRow.append(indexData);
+  newRow.append(itemData);
+
+  table.append(newRow);
+}
+
+function removeItem() {
+  const regex = new RegExp("[0-9]{1}");
+  var tableItems = document.querySelectorAll("td");
+  console.log(tableItems);
+
+  var removalInput = document.getElementById("removalInput").value;
+  for (i = 0; i < tableItems.length; i++) {
+    console.log(tableItems[i].innerText);
+    if (tableItems[i].innerText === removalInput) {
+      tableItems[i].remove();
+      tableItems[i + 1].remove();
+    }
+  }
+  for (i = 0; i < tableItems.length; i++) {
+    if (
+      regex.test(tableItems[i].innerText) &&
+      tableItems[i].innerText > removalInput
+    ) {
+      tableItems[i].innerText = tableItems[i].innerText - 1;
+    }
+  }
+
+  removalInput = removalInput - 1;
+
+  console.log(items[removalInput]);
+  console.log(items);
+
+  items.splice(removalInput, 1);
+  console.log(items);
+}
+
+createItem();
